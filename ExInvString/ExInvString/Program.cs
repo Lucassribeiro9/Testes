e5153d73bf2383c1ce6ae0e8d997df584415dbe0
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-03-27 19:08:12 -0300

    resolvido

--- ExInvString/ExInvString/Program.cs
+++ ExInvString/ExInvString/Program.cs
@@ -5,10 +5,20 @@ IMPORTANTE:
 a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
 b) Evite usar funções prontas, como, por exemplo, reverse;
  */
+using System.Text;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string s = "Teste Técnico";
+        StringBuilder sb = new StringBuilder();
+
+        for (var i = s.Length -1; i >= 0; i--)
+        {
+            sb.Append(s[i]);
+        }
+        Console.WriteLine($"Normal: {s}");
+        Console.WriteLine($"Invertido: {sb.ToString()}");
     }
 }
