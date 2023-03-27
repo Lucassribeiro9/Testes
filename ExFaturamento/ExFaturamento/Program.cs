179b4a572f55902ef4a1b82a019e332bfdd539e6
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-03-27 18:56:31 -0300

    enunciado

--- ExFaturamento/ExFaturamento/Program.cs
+++ ExFaturamento/ExFaturamento/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+ 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
+• O menor valor de faturamento ocorrido em um dia do mês;
+• O maior valor de faturamento ocorrido em um dia do mês;
+• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+
+ */
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
