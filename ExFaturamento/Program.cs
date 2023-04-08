0db94170a3af1e2ac96cd087854286f0230d17ed
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-04-08 17:52:24 -0300

    exercicio reformulado

--- ExFaturamento/Program.cs
+++ ExFaturamento/Program.cs
@@ -1,22 +1,26 @@
-﻿using ExFaturamento;
-using System;
-using System.Diagnostics;
+﻿using System;
 
-    Faturamento[] vect = new Faturamento[5];
-    Console.Write("Quantidade de dias: ");
-    int n = Convert.ToInt32(Console.ReadLine());
-    for (int i = 1; i <= n; i++)
-        {
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Quantidade de dias: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Faturamento[] vect = new Faturamento[n];
+
+        for (int i = 1; i <= n; i++)
+         {
             Console.Write("Digite o dia:");
             int dia = Convert.ToInt32(Console.ReadLine());
             Console.Write("Digite o faturamento: ");
             double faturamento = Convert.ToDouble(Console.ReadLine());
             vect[n] = new Faturamento(faturamento, dia);
-        }
-    Console.WriteLine();
-    Console.WriteLine("Dias faturados: ");
-    foreach (Faturamento obj in vect)
-        {
-            Console.WriteLine(obj);
-        }
-   
+         }
+        Console.WriteLine();
+        Console.WriteLine("Dias faturados: ");
+        foreach (Faturamento obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+    }
+}
---
+++ ExNFaturamento/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using ExNFaturamento;
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Quantidade de dias: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        List<Faturamento> list = new List<Faturamento>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            System.Console.WriteLine($"Dia {i}:");
+            int dia = 0;
+            dia += i;
+            System.Console.Write($"Total faturado: ");
+            double faturadoDia = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            list.Add(new Faturamento(dia, faturadoDia));
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine("Faturado: ");
+        foreach (Faturamento item in list)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+}
