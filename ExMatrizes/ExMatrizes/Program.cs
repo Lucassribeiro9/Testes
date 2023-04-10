bcc630cc1e2ca4f183a41c855f8755bcfd797837
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-04-10 16:22:37 -0300

    matrizes

---
+++ ExMatrizes/ExMatrizes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ExMatrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantidade de elementos: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n,n];
+
+            for (int i = 0;i < n;i++)
+            {
+                string[] espaco = Console.ReadLine().Split(' ');
+                for(int j = 0;j < n;j++)
+                {
+                    mat[i,j] = int.Parse(espaco[j]);
+                }
+            }
+        }
+    }
+
+}
