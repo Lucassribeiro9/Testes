37f6ea18331bdde096912e85f1a868c93d60e886
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-04-10 16:39:00 -0300

    procurando uma maneira de organizar a matriz

--- ExMatrizes/ExMatrizes/Program.cs
+++ ExMatrizes/ExMatrizes/Program.cs
@@ -1,23 +1,30 @@
-﻿using System;
-namespace ExMatrizes
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Quantidade de elementos: ");
-            int n = int.Parse(Console.ReadLine());
-            int[,] mat = new int[n,n];
+﻿Console.Write("Quantidade de elementos: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] mat = new int[n, n];
 
-            for (int i = 0;i < n;i++)
-            {
-                string[] espaco = Console.ReadLine().Split(' ');
-                for(int j = 0;j < n;j++)
-                {
-                    mat[i,j] = int.Parse(espaco[j]);
-                }
-            }
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        mat[i, j] = int.Parse(espaco[j]);
+    }
+}
+Console.WriteLine("Diagonal: ");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(mat[i, i] + " ");
+}
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (mat[i, j] < 0)
+        {
+            count++;
         }
     }
-
 }
+Console.WriteLine($"Número negativo: {count}");
