099ddf0c000152c2592652e54545a1b2166cbc54
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-04-11 15:08:13 -0300

    erro em imprimir matriz

--- ExMatrizes/ExMatrizes/Program.cs
+++ ExMatrizes/ExMatrizes/Program.cs
@@ -1,30 +1,26 @@
-﻿Console.Write("Quantidade de elementos: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] mat = new int[n, n];
-
-for (int i = 0; i < n; i++)
+﻿namespace ExMatrizes
 {
-    for (int j = 0; j < n; j++)
+    class Program
     {
-        mat[i, j] = int.Parse(espaco[j]);
-    }
-}
-Console.WriteLine("Diagonal: ");
-for (int i = 0; i < n; i++)
-{
-    Console.WriteLine(mat[i, i] + " ");
-}
-Console.WriteLine();
-
-int count = 0;
-for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        if (mat[i, j] < 0)
+        static void Main(string[] args)
         {
-            count++;
+            Console.Write("Quantidade de elementos: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(String.Format("Informe o elemento ({0},{1}): ", i, j));
+                    mat[i, j] = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(mat[i, j]);
+
+                }
+            }
+
+
         }
     }
+
 }
-Console.WriteLine($"Número negativo: {count}");
