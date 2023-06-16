dbe5a908c5c043f6ef28b076c2445945bd0b3ea9
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-16 20:02:51 -0300

    criacao atributos e heranca CarroEletrico

---
+++ CarrosEx/CarroEletrico.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarrosEx
+{
+    public class CarroEletrico : Carro
+    {
+        public int Potencia { get; set; }
+        public int DuracaoBateria { get; set; }
+    }
+}
