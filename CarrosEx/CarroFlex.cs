9a9847736ef018da46424dbf964e12c1a078cad8
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-16 20:02:35 -0300

    criacao e atributos de CarroFlex

---
+++ CarrosEx/CarroFlex.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarrosEx
+{
+    public class CarroFlex
+    {
+        public int NumPortas { get; set; }
+        public int NumCilindradas { get; set; }
+    }
+}
