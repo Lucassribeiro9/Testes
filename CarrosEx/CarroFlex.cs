f0f2d06788a9056ada73a570efcc3c8ce1682345
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-16 20:40:43 -0300

    add CalcularCustoVenda

--- CarrosEx/CarroFlex.cs
+++ CarrosEx/CarroFlex.cs
@@ -9,5 +9,12 @@ namespace CarrosEx
     {
         public int NumPortas { get; set; }
         public decimal NumCilindradas { get; set; }
+
+        public override decimal CalcularCustoVenda()
+        {
+            decimal imposto = CustoProducao * 0.2m;
+            decimal lucro = CustoProducao * 0.25m;
+            return CustoProducao + imposto + lucro;
+        }
     }
 }
