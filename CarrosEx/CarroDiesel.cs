7d2ab94767d66860026848bee552f0a857dedad8
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-16 20:02:13 -0300

    criacao e atributos de CarroDiesel

---
+++ CarrosEx/CarroDiesel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarrosEx
+{
+    public class CarroDiesel
+    {
+        public int CapacidadeCarga { get; set; }
+        public int VolumeCacamba { get; set; }
+    }
+}
