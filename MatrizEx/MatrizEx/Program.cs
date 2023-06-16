fa8720cfe8ee2dc335622fb60c42c1b548d89e16
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-16 19:41:24 -0300

    checagem de pares e impares

--- MatrizEx/MatrizEx/Program.cs
+++ MatrizEx/MatrizEx/Program.cs
@@ -4,19 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Quantidade de elementos: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[,] mat = new int[n,n];
+            Console.Write("Quantidade de linhas: ");
+            int linhas = int.Parse(Console.ReadLine());
+            Console.Write("Quantidade de colunas: ");
+            int colunas = int.Parse(Console.ReadLine());
+            int[,] mat = new int[linhas, colunas];
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < linhas; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < colunas; j++)
                 {
-                    Console.Write(String.Format("Informe o elemento ({0},{1}): ", i, j));
-                    mat[i, j] = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(mat[i, j]);
+                    Console.Write($"Digite o elemento [{i}, {j}]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
 
                 }
+            }
+            int par = 0;
+            int impar = 0;
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    if (mat[i, j] % 2 == 0)
+                    {
+                        par++;
+                    }
+                    else
+                    {
+                        impar++;
+                    }
+                }
+            }
+            if (par > impar)
+            {
+                Console.WriteLine("Quantidade de números pares: " + par);
+
+            }
+            else
+            {
+                Console.WriteLine("Quantidade de números ímpares: " + impar);
+
             }
         }
     }
