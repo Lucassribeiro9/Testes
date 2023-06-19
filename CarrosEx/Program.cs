61fcbe64c48d208c39f4334881e3d66b182db24c
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-19 10:06:21 -0300

    adicionando programa de cadastro

--- CarrosEx/Program.cs
+++ CarrosEx/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarrosEx;
+
+Console.WriteLine("Sistema de cadastro de carros");
+Console.Write("Digite o número do motor: ");
+string motor = Console.ReadLine();
+Console.Write("Digite o chassi: ");
+string chassi = Console.ReadLine();
+Console.Write("Digite o custo da produção: ");
+decimal custoProducao = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Qual o tipo do carro? Flex, Diesel ou Elétrico? ");
+string tipoCarro = Console.ReadLine();
+
