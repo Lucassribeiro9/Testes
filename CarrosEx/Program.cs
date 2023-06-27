6abdb761e426d9f63299103f41dc28e830f3ebc3
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-06-27 10:35:23 -0300

    Adiciona verificação de campos obrigatórios na entrada do usuário

--- CarrosEx/Program.cs
+++ CarrosEx/Program.cs
@@ -4,56 +4,117 @@ using System.Linq;
 using System.Threading.Tasks;
 using CarrosEx;
 
-Console.WriteLine("Sistema de cadastro de carros");
-Console.Write("Digite o número do motor: ");
-string motor = Console.ReadLine();
-Console.Write("Digite o chassi: ");
-string chassi = Console.ReadLine();
-Console.Write("Digite o custo da produção: ");
-decimal custoProducao = Convert.ToDecimal(Console.ReadLine());
-Console.Write("Qual o tipo do carro? Flex, Diesel ou Elétrico? ");
-string tipoCarro = Console.ReadLine();
-
-switch (tipoCarro)
+bool continuar = true;
+while (continuar)
 {
-    case "Flex":
-        Console.Write($"Digite o número de portas: ");
-        int numPortas = Convert.ToInt32(Console.ReadLine());
-        Console.Write($"Digite o número de cilindradas: ");
-        decimal numCilindradas = Convert.ToDecimal(Console.ReadLine());
-        CarroFlex carro = new CarroFlex(chassi, motor, custoProducao, numPortas, numCilindradas);
-        Console.WriteLine($"Valor obtido: {carro.CalcularCustoVenda()}");
-        break;
+    Console.WriteLine("Sistema de cadastro de carros");
 
-    case "Diesel":
-        Console.Write($"Capacidade de carga: ");
-        decimal capacidadeCarga = Convert.ToDecimal(Console.ReadLine());
-        Console.Write($"Volume de cacamba: ");
-        decimal volumeCacamba = Convert.ToDecimal(Console.ReadLine());
-        CarroDiesel carroDiesel = new CarroDiesel(
-            chassi,
-            motor,
-            custoProducao,
-            capacidadeCarga,
-            volumeCacamba
-        );
-        Console.WriteLine($"Valor obtido: {carroDiesel.CalcularCustoVenda()}");
-        break;
-    case "Elétrico":
-        Console.Write($"Potência: ");
-        decimal potencia = Convert.ToDecimal(Console.ReadLine());
-        Console.Write($"Duração da bateria: ");
-        decimal duracaoBateria = Convert.ToDecimal(Console.ReadLine());
-        CarroEletrico carroEletrico = new CarroEletrico(
-            chassi,
-            motor,
-            custoProducao,
-            potencia,
-            duracaoBateria
-        );
-        Console.WriteLine($"Valor obtido: {carroEletrico.CalcularCustoVenda()}");
-        break;
-    default:
-        Console.WriteLine($"Texto inválido");
-        break;
+    string motor = "";
+    while (motor == "")
+    {
+        Console.Write("Digite o número do motor: ");
+        motor = Console.ReadLine();
+
+        if (motor == "")
+        {
+            Console.WriteLine("Erro: Campo obrigatório. Digite o número do motor novamente.");
+        }
+    }
+
+    string chassi = "";
+    while (chassi == "")
+    {
+        Console.Write("Digite o chassi: ");
+        chassi = Console.ReadLine();
+
+        if (chassi == "")
+        {
+            Console.WriteLine("Erro: Campo obrigatório. Digite o chassi novamente.");
+        }
+    }
+
+    decimal custoProducao = 0;
+    bool entradaValida = false;
+    while (!entradaValida)
+    {
+        Console.Write("Digite o custo da produção: ");
+
+        try
+        {
+            custoProducao = Convert.ToDecimal(Console.ReadLine());
+            entradaValida = true;
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Erro: Entrada inválida. Digite um número válido.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erro: {e.Message}");
+        }
+    }
+
+    string tipoCarro = "";
+    while (tipoCarro != "Flex" && tipoCarro != "Diesel" && tipoCarro != "Elétrico")
+    {
+        Console.Write("Qual o tipo do carro? Flex, Diesel ou Elétrico? ");
+        tipoCarro = Console.ReadLine();
+        switch (tipoCarro)
+        {
+            case "Flex":
+                int numPortas = 0;
+                while (numPortas == 0)
+                {
+                    Console.Write($"Digite o número de portas: ");
+                    int numPortas = Convert.ToInt32(Console.ReadLine());
+
+                }
+                Console.Write($"Digite o número de cilindradas: ");
+                decimal numCilindradas = Convert.ToDecimal(Console.ReadLine());
+                CarroFlex carro = new CarroFlex(chassi, motor, custoProducao, numPortas, numCilindradas);
+                Console.WriteLine($"Valor obtido: {carro.CalcularCustoVenda()}");
+                break;
+
+            case "Diesel":
+                Console.Write($"Capacidade de carga: ");
+                decimal capacidadeCarga = Convert.ToDecimal(Console.ReadLine());
+                Console.Write($"Volume de cacamba: ");
+                decimal volumeCacamba = Convert.ToDecimal(Console.ReadLine());
+                CarroDiesel carroDiesel = new CarroDiesel(
+                    chassi,
+                    motor,
+                    custoProducao,
+                    capacidadeCarga,
+                    volumeCacamba
+                );
+                Console.WriteLine($"Valor obtido: {carroDiesel.CalcularCustoVenda()}");
+                break;
+            case "Elétrico":
+                Console.Write($"Potência: ");
+                decimal potencia = Convert.ToDecimal(Console.ReadLine());
+                Console.Write($"Duração da bateria: ");
+                decimal duracaoBateria = Convert.ToDecimal(Console.ReadLine());
+                CarroEletrico carroEletrico = new CarroEletrico(
+                    chassi,
+                    motor,
+                    custoProducao,
+                    potencia,
+                    duracaoBateria
+                );
+                Console.WriteLine($"Valor obtido: {carroEletrico.CalcularCustoVenda()}");
+                break;
+            default:
+                Console.WriteLine($"Texto inválido");
+                break;
+        }
+    }
+
+    Console.Write("Deseja continuar? (S/N) ");
+    string resposta = Console.ReadLine();
+
+    if (resposta.ToUpper() != "S")
+    {
+        continuar = false;
+    }
 }
+
