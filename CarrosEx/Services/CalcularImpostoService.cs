fc992de277f9bc4dcccc6b5cd986e64b3febf0e4
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-07-08 18:59:15 -0300

    criação dos métodos de calcular custo venda

--- CarrosEx/Services/CalcularImpostoService.cs
+++ CarrosEx/Services/CalcularImpostoService.cs
@@ -7,10 +7,22 @@ namespace CarrosEx.Services
 {
     public class CalcularImpostoService
     {
-         public virtual decimal CalcularCustoVenda(decimal custoProducao)
+        public virtual decimal CalcularCustoVenda(decimal custoProducao)
         {
             decimal imposto = custoProducao * 0.2m;
             decimal lucro = custoProducao * 0.25m;
+            return custoProducao + imposto + lucro;
+        }
+        public virtual decimal CalcularCustoVendaCarroDiesel(decimal custoProducao)
+        {
+            decimal imposto = custoProducao * 0.3m;
+            decimal lucro = custoProducao * 0.3m;
+            return custoProducao + imposto + lucro;
+        }
+        public virtual decimal CalcularCustoVendaCarroEletrico(decimal custoProducao)
+        {
+            decimal imposto = custoProducao * 0.1m;
+            decimal lucro = custoProducao * 0.2m;
             return custoProducao + imposto + lucro;
         }
     }
